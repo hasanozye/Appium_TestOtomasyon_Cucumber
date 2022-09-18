ed465730a09e10984f274c725923764bde300668
Mete <hasanozyer06@gmail.com>
2022-09-18 03:05:45 +0300

    a101 appium browserstack cucumber the holy triangle stuff

--- src/test/java/AppiumTest/Pages/A101Page.java
+++ src/test/java/AppiumTest/Pages/A101Page.java
@@ -1,13 +1,17 @@
 package AppiumTest.Pages;
 
 import AppiumTest.utulities.Driver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class A101Page {
     public A101Page() {
         PageFactory.initElements(new AppiumFieldDecorator(Driver.get()), this);
     }
+    @AndroidFindBy(xpath = "BRUH...")
+    public WebElement justDOIT;
 
 
 }
